ad8ac63e764632e7d04c25e79d2d19c6bd697a48
Vishal Das <vdas53073@gmail.com>
2023-06-04 02:07:24 +0530

    v1.0

--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -4,81 +4,75 @@ import "./Test.css"
 import MyTimer from '../../components/Timer/Timer';
 
 
-
-
 const Test = () => {
-    const navigate = useNavigate();
-    const para = useParams();
-    const duration = (para.type.split(" ")[0])*60;
-    const [showalert, setShowalert] = useState(false);
-    const keys = ['a','s','d','f','j','k','l'];
-    const [initialkeys, setInitialkeys] = useState([]);
-    const [accuracy,setAccuracy] = useState(0);
-    const [inputdisabled, setInputdisabled] = useState(false);
-    const [speed, setSpeed] = useState(0);
-    const [index,setIndex] = useState(0)
-    const initial = useRef(false);
-    const typekey = useRef("")
-    const correct = useRef(0);
-    const wrong = useRef(0);
+  const navigate = useNavigate();
+  const para = useParams();
+  const duration = (para.type.split(" ")[0])*60;
+  const keys = ['a','s','d','f','j','k','l'];
+  const [initialkeys, setInitialkeys] = useState([]);
+  const [initial, setinitial] = useState(false);
+  const [inputdisabled, setInputdisabled] = useState(false);
+  const [showalert, setShowalert] = useState(false);
+  const [speed, setSpeed] = useState(0);
+  const [accuracy,setAccuracy] = useState(0);
+ const typekey = useRef();
+ const correct = useRef(0);
+ const wrong = useRef(0);
+  const [index,setIndex] = useState(0)
 
-    const add = ()=>{
-        setInitialkeys(prev => [...prev,keys[Math.floor(Math.random()*(keys.length))]]);
+  const add = ()=>{
+    setInitialkeys(prev => [...prev,keys[Math.floor(Math.random()*(keys.length))]]);
+   }
+
+useEffect(() => {
+    if(initial === false){
+        setinitial(true);
+    for(let i = 0;i<12;i++){
+            add();
     }
-    useEffect(() => {
-        if(initial.current === false){
-            initial.current = true;
-        for(let i = 0;i<12;i++){
-                add();
-        }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+}
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
 
-
-
-    const check = (e)=>{
-        typekey.current = e.target.value;
+const check = (e)=>{
+  typekey.current = e.target.value;
         if(typekey.current === ""){}
-        else if(typekey.current === initialkeys[index]){
-             correct.current +=1;
-            
-             let key_elem = document.getElementById(`${index}`)
-             key_elem.style.backgroundColor = "green";
-             setIndex(prev => prev+1);
-             add();
-            
-        }
-        else if(typekey.current !==  initialkeys[index]){
-          wrong.current+=1;
-        
-          let key_elem = document.getElementById(`${index}`)
-          key_elem.style.backgroundColor = "red";
-          setIndex(prev => prev+1);
-          //adding key
-          add();
-          console.log("wrong")
-        }
-        //  console.log(e.nativeEvent.data);
-       }
+  else if(typekey.current === initialkeys[index]){
+       console.log("correct")
+      let elem_key = document.getElementById(`${index}`)
+    
+      elem_key.style.backgroundColor = "green";
+      setIndex(prev => prev+1)
+      correct.current+=1;
+  }
+  else if (typekey.current !==  initialkeys[index]){
+     console.log("Wrong");
+     let elem_key = document.getElementById(`${index}`)
+     
+      elem_key.style.backgroundColor = "red";
+      setIndex(prev => prev+1)
+      wrong.current+=1;
+  }
+}
 
-       const checkAccuracy = ()=>{
-        const result = (parseFloat(correct.current)/(parseFloat(correct.current) + parseFloat(wrong.current)))*(100.00);
-        setAccuracy(result.toFixed(2));
-      }
+const checkAccuracy = ()=>{
+  const result = (parseFloat(correct.current)/(parseFloat(correct.current) + parseFloat(wrong.current)))*(100.00);
+  setAccuracy(result.toFixed(2));
+}
 
-      useEffect(()=>{
-        checkAccuracy();
-         // eslint-disable-next-line react-hooks/exhaustive-deps
-      },[correct.current,wrong.current])
+useEffect(()=>{
+  checkAccuracy();
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+},[correct.current,wrong.current])
 
-      let textarea = document.getElementById('keys_box');
+let textarea = document.getElementById('keys_box');
       if(textarea?.scrollHeight)
        textarea.scrollTop = textarea.scrollHeight;
 
       const time = new Date();
       time.setSeconds(time.getSeconds() + duration); 
-      
+
+    
       const finish = ()=>{
         setShowalert(false);
         navigate("/");
@@ -86,42 +80,41 @@ const Test = () => {
 
   return (
     <div className='test'>
-    { <>
-        <div className="container">
-            <h2 id='test_title'>Analyze your typing speed in {para.type}</h2>
-            <MyTimer expiryTimestamp={time} duration={duration} setInputdisabled={setInputdisabled} correct={correct.current}
+            {<div className="container">
+                   <h2>Analyze your typing speed in {para.type}</h2>
+                   <MyTimer expiryTimestamp={time} duration={duration} setInputdisabled={setInputdisabled} correct={correct.current}
                    wrong={wrong.current} accuracy={accuracy} setShowalert={setShowalert} showalert={showalert} setSpeed={setSpeed}/>
-            <div className="text_info">
-                <span id='correct_info'>Correct keys:- {correct.current}</span>
-                <span id='wrong_info'>Wrong keys:- {wrong.current}</span>
-                <span id='accuracy_info'>Accuracy:- {accuracy}%</span>
-            </div>
-            <div id="keys_box">
-                {initialkeys.map((ele,i)=>(
-                    <span className="text" id={i} key={Math.random()}>{ele}</span>
-                ))
-               }
-            </div>
-            <input type="text" ref={typekey} onChange={(e)=>check(e)} id='input' disabled={inputdisabled}/>
-           
-        </div>
-        </>}
-        {showalert &&  <>
-                 <div className="alert">
-                    <div id="alert_container">
-                      <h3>Congratulation on completing the test</h3>
-                      <h4>Result:-</h4>
-                      <span id='correct'>Correct keys:- {correct.current}</span>
-                      <span id="wrong">Wrong keys:- {wrong.current}</span>
-                      <span id='speed'>Speed:- {speed} wpm</span>
-                      <span id='accuracy'>Accuracy:- {accuracy}</span>
-                      <span id='close' onClick={()=>finish()}>{"x"}</span>
-                    </div>
-                 
-                 </div>
+                   <div className="text_info">
+                   <span id='correct_info'>Correct keys:- {correct.current}</span>
+                   <span id='wrong_info'>Wrong keys:- {wrong.current}</span>
+                    <span id='accuracy_info'>Accuracy:- {accuracy}%</span>
+                </div>
+                   <div id="keys_box">
+                   {initialkeys.map((ele,i)=>(
+                     <span className="text" id={i} key={i}>{ele}</span>
+                   ))
+                   }
+                   </div>
 
+                   <input type="text" ref = {typekey} onChange={(e)=>check(e)} id='input' disabled={inputdisabled}/>
+            </div>
+          }
+          {showalert &&  <>
+            <div className="alert">
+               <div id="alert_container">
+                 <h3>Congratulation on completing the test</h3>
+                 <h4>Result:-</h4>
+                 <span id='correct'>Correct keys:- {correct.current}</span>
+                 <span id="wrong">Wrong keys:- {wrong.current}</span>
+                 <span id='speed'>Speed:- {speed} wpm</span>
+                 <span id='accuracy'>Accuracy:- {accuracy}</span>
+                 <span id='close' onClick={()=>finish()}>{"x"}</span>
+               </div>
             
-            </>}
+            </div>
+
+       
+       </>}
     </div>
   )
 }
---
+++ src/Pages/Test/Test2.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import "./Test.css"
+import MyTimer from '../../components/Timer/Timer';
+
+
+
+
+const Test = () => {
+    const navigate = useNavigate();
+    const para = useParams();
+    const duration = (para.type.split(" ")[0])*60;
+    const [showalert, setShowalert] = useState(false);
+    const keys = ['a','s','d','f','j','k','l'];
+    const [initialkeys, setInitialkeys] = useState([]);
+    const [accuracy,setAccuracy] = useState(0);
+    const [inputdisabled, setInputdisabled] = useState(false);
+    const [speed, setSpeed] = useState(0);
+    const [index,setIndex] = useState(0)
+    const initial = useRef(false);
+    const typekey = useRef("")
+    const correct = useRef(0);
+    const wrong = useRef(0);
+
+    const add = ()=>{
+        setInitialkeys(prev => [...prev,keys[Math.floor(Math.random()*(keys.length))]]);
+    }
+    useEffect(() => {
+        if(initial.current === false){
+            initial.current = true;
+        for(let i = 0;i<12;i++){
+                add();
+        }
+    }
+   
+    }, []);
+
+
+
+    const check = (e)=>{
+        typekey.current = e.target.value;
+        if(typekey.current === ""){}
+        else if(typekey.current === initialkeys[index]){
+             correct.current +=1;
+            
+            //  let key_elem = document.getElementById(`${index}`)
+            //  key_elem.style.backgroundColor = "green";
+           
+             setIndex(prev => prev+1);
+             add();
+            
+        }
+        else if(typekey.current !==  initialkeys[index]){
+          wrong.current+=1;
+        
+        //   let key_elem = document.getElementById(`${index}`)
+        
+        // key_elem.classList.add("wrong_key");
+       
+          // key_elem.style.backgroundColor = "orange";
+          setIndex(prev => prev+1);
+          //adding key
+          add();
+          console.log("wrong")
+        }
+    
+       }
+
+       const checkAccuracy = ()=>{
+        const result = (parseFloat(correct.current)/(parseFloat(correct.current) + parseFloat(wrong.current)))*(100.00);
+        setAccuracy(result.toFixed(2));
+      }
+
+      useEffect(()=>{
+        checkAccuracy();
+         // eslint-disable-next-line react-hooks/exhaustive-deps
+      },[correct.current,wrong.current])
+
+      let textarea = document.getElementById('keys_box');
+      if(textarea?.scrollHeight)
+       textarea.scrollTop = textarea.scrollHeight;
+
+      const time = new Date();
+      time.setSeconds(time.getSeconds() + duration); 
+      
+      const finish = ()=>{
+        setShowalert(false);
+        navigate("/");
+      }
+
+  return (
+    <div className='test'>
+    { <>
+        <div className="container">
+            <h2 id='test_title'>Analyze your typing speed in {para.type}</h2>
+            <MyTimer expiryTimestamp={time} duration={duration} setInputdisabled={setInputdisabled} correct={correct.current}
+                   wrong={wrong.current} accuracy={accuracy} setShowalert={setShowalert} showalert={showalert} setSpeed={setSpeed}/>
+            <div className="text_info">
+                <span id='correct_info'>Correct keys:- {correct.current}</span>
+                <span id='wrong_info'>Wrong keys:- {wrong.current}</span>
+                <span id='accuracy_info'>Accuracy:- {accuracy}%</span>
+            </div>
+            <div id="keys_box">
+                {initialkeys.map((ele,i)=>(
+                    <span className={`text`} id={i} key={Math.random()}>{ele}</span>
+                ))
+               }
+            </div>
+            <input type="text" ref={typekey} onChange={(e)=>check(e)} id='input' disabled={inputdisabled}/>
+           
+        </div>
+        </>}
+        {showalert &&  <>
+                 <div className="alert">
+                    <div id="alert_container">
+                      <h3>Congratulation on completing the test</h3>
+                      <h4>Result:-</h4>
+                      <span id='correct'>Correct keys:- {correct.current}</span>
+                      <span id="wrong">Wrong keys:- {wrong.current}</span>
+                      <span id='speed'>Speed:- {speed} wpm</span>
+                      <span id='accuracy'>Accuracy:- {accuracy}</span>
+                      <span id='close' onClick={()=>finish()}>{"x"}</span>
+                    </div>
+                 
+                 </div>
+
+            
+            </>}
+    </div>
+  )
+}
+
+export default Test
