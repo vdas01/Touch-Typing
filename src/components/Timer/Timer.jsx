f358f5103fde460525ffe530f258644aeca1df98
Vishal Das <vdas53073@gmail.com>
2023-06-03 21:54:32 +0530

    v1.0

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -15,6 +15,7 @@ export default function MyTimer({ expiryTimestamp,duration,setInputdisabled ,cor
   
   useEffect(() => {
     setInputdisabled(!isRunning);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isRunning]);
 
     return (
